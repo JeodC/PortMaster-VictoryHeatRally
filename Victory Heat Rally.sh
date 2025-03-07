9b8b975839775c5f8bbc75199d7ac0cd47fee552
Jeod <47716344+JeodC@users.noreply.github.com>
2025-03-07 11:12:10 -0500

    Fix pixel scale option inconsistency and progression bug

--- Victory Heat Rally.sh
+++ Victory Heat Rally.sh
@@ -31,11 +31,6 @@ export PATCHER_GAME="$(basename "${0%.*}")" # This gets the current script filen
 export PATCHER_TIME="2 to 5 minutes"
 export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
 
-# dos2unix in case we need it
-dos2unix "$GAMEDIR/tools/gmKtool.py"
-dos2unix "$GAMEDIR/tools/Klib/GMblob.py"
-dos2unix "$GAMEDIR/tools/patchscript"
-
 # Check if patchlog.txt to skip patching
 if [ ! -f patchlog.txt ]; then
     if [ -f "$controlfolder/utils/patcher.txt" ]; then
@@ -55,9 +50,9 @@ if [ -f "$GAMEDIR/patchlog.txt" ]; then
 fi
 
 # Assign gptokeyb and load the game
-$GPTOKEYB "gmloader.aarch64" -c "vhr.gptk" &
-pm_platform_helper "gmloader.aarch64"
-./gmloader.aarch64 -c "gmloader.json"
+$GPTOKEYB "gmloadernext.aarch64" -c "vhr.gptk" &
+pm_platform_helper "gmloadernext.aarch64" >/dev/null
+./gmloadernext.aarch64 -c "gmloader.json"
 
 # Cleanup
 pm_finish
